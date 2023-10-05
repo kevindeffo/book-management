6ade2fe5c92d61dd6fabeed570b37c2bb03807f1
kevindeffo <deffokevin14@gmail.com>
2023-10-05 12:18:02 +0100

    End with basic CRUD features

--- app.js
+++ app.js
@@ -1,26 +1,51 @@
 const app = require('./server')
 const data = require('./datas')
+const books = data.books;
 
 
+//CREATE NEW BOOK
 app.post('/api/save', (req, res) => {
+    books.push(req.body);
     res.status(200).json("book save successfully");
-})
+});
 
 
-app.put('/api/:id', (req, res) => {
-    res.status(200).json("book update successfully");
-})
-app.get('/api', (req, res) => {
-
-    const books = data.books
-    res.status(200).json({books});
-})
-
+//GET A BOOK WITH HIS ID
 app.get('/api/:id', (req, res) => {
-    book ={}
-    res.status(200).json({book})
-})
+    const id = req.params.id
+    let book ={}
 
-app.delete('api/:id', (req, res)=>{
+    //GET  ID OF THE ELEMENT
+    let index = books.findIndex(book=> book.id.toString() === id.toString())
+    book = books[index];
+    res.status(200).json({book});
+});
 
-})
+
+//GET ALL THE BOOKS
+app.get('/api', (req, res) => {
+    res.status(200).json({books});
+});
+
+
+//UPDATE AN EXISTING BOOK
+app.put('/api/:id', (req, res) => {
+    const id = req.params.id
+
+    //GET  ID OF THE ELEMENT TO UPDATE
+    let index = books.findIndex(book => book.id.toString() === id.toString());
+    books[index] = req.body;
+    res.status(200).json({message: "book update successfully", book: req.body});
+});
+
+
+//DELETE A BOOK
+app.delete('/api/:id', (req, res)=>{
+    const id = req.params.id
+
+    //GET  ID OF THE ELEMENT TO DELETE
+    let index = books.findIndex(book=> book.id.toString() === id.toString())
+
+    books.splice(index, 1);
+    res.status(200).json({message: "book delete successfully"});
+});
--- server.js
+++ server.js
@@ -8,21 +8,9 @@ const port = 4000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-    );
-    next();
-});
 
 
-
+//Start application
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
